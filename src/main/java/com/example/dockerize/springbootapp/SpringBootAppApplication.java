61f119e1164666c725e3f42d5aa22f51ef9b2363
Burela Sai Likhith <sai.burela@prodevans.com>
2024-01-23 11:01:20 +0530

    First Message

---
+++ src/main/java/com/example/dockerize/springbootapp/SpringBootAppApplication.java
@@ -0,0 +1,24 @@
+package com.example.dockerize.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+
+	@GetMapping()
+	public String getGreeting() {
+		return "Hello Welcome to my World.......... Sometimes the impossible task is also not so possibly Impossible";
+	}
+
+}
